49e4eead5b01d2f3f75feb84aa9914167acf4268
Pawel Janas <pjanas@br013.ib.bridges2.psc.edu>
2025-07-10 05:19:58 -0400

    Removed MORE redundant loops
    No parallelism yet.

--- C/src/main.c
+++ C/src/main.c
@@ -30,11 +30,13 @@ double max_diff = 0.0;
 double min_diff = 1.0;
 double total_diff = 0.0;
 
+omp_set_num_threads(omp_get_max_threads); 
+
 void initialize_graph(void)
 {
-    for(int i = 0; i < GRAPH_ORDER; i++)
+    for (int i = 0; i < GRAPH_ORDER; i++)
     {
-        for(int j = 0; j < GRAPH_ORDER; j++)
+        for (int j = 0; j < GRAPH_ORDER; j++)
         {
             adjacency_matrix[i][j] = 0.0;
         }
@@ -59,28 +61,26 @@ void calculate_pagerank(double pagerank[])
     double time_per_iteration = 0;
     double new_pagerank[GRAPH_ORDER];
 
-    for(int i = 0; i < GRAPH_ORDER; i++)
+    for (int i = 0; i < GRAPH_ORDER; i++)
     {
         pagerank[i] = initial_rank;
-	new_pagerank[i] = 0.0;
+        new_pagerank[i] = 0.0;
     }
 
     // If we exceeded the MAX_TIME seconds, we stop. If we typically spend X seconds on an iteration, and we are less than X seconds away from MAX_TIME, we stop.
-    while(elapsed < MAX_TIME && (elapsed + time_per_iteration) < MAX_TIME)
+    while (elapsed < MAX_TIME && (elapsed + time_per_iteration) < MAX_TIME)
     {
         double iteration_start = omp_get_wtime();
-
-        for(int i = 0; i < GRAPH_ORDER; i++)
+        for (int i = 0; i < GRAPH_ORDER; i++)
         {
-            new_pagerank[i] = 0.0;
-        
-            for(int j = 0; j < GRAPH_ORDER; j++)
+            new_pagerank[i] = 0.0; // removed abpve for-loop with this.
+            for (int j = 0; j < GRAPH_ORDER; j++)
             {
                 if (adjacency_matrix[j][i] == 1.0)
                 {
                     int outdegree = 0;
 
-                    for(int k = 0; k < GRAPH_ORDER; k++)
+                    for (int k = 0; k < GRAPH_ORDER; k++)
                     {
                         if (adjacency_matrix[j][k] == 1.0)
                         {
@@ -92,13 +92,13 @@ void calculate_pagerank(double pagerank[])
             }
         }
 
-        for(int i = 0; i < GRAPH_ORDER; i++)
+        for (int i = 0; i < GRAPH_ORDER; i++)
         {
             new_pagerank[i] = DAMPING_FACTOR * new_pagerank[i] + damping_value;
         }
 
         diff = 0.0;
-        for(int i = 0; i < GRAPH_ORDER; i++)
+        for (int i = 0; i < GRAPH_ORDER; i++)
         {
             diff += fabs(new_pagerank[i] - pagerank[i]);
         }
@@ -106,27 +106,29 @@ void calculate_pagerank(double pagerank[])
         total_diff += diff;
         min_diff = (min_diff > diff) ? diff : min_diff;
 
-        double pagerank_total = 0.0;
-
-	for(int i = 0; i < GRAPH_ORDER; i++)
+        for (int i = 0; i < GRAPH_ORDER; i++)
         {
             pagerank[i] = new_pagerank[i];
-            pagerank_total += pagerank[i];
         }
 
-        if(fabs(pagerank_total - 1.0) >= 1E-12)
+        double pagerank_total = 0.0;
+        for (int i = 0; i < GRAPH_ORDER; i++)
+        {
+            pagerank_total += pagerank[i];
+        }
+        if (fabs(pagerank_total - 1.0) >= 1E-12)
         {
             printf("[ERROR] Iteration %zu: sum of all pageranks is not 1 but %.12f.\n", iteration, pagerank_total);
         }
 
-		double iteration_end = omp_get_wtime();
-		elapsed = omp_get_wtime() - start;
-		iteration++;
-		time_per_iteration = elapsed / iteration;
+        double iteration_end = omp_get_wtime();
+        elapsed = omp_get_wtime() - start;
+        iteration++;
+        time_per_iteration = elapsed / iteration;
     }
 
     printf("%zu iterations achieved in %.2f seconds\n", iteration, elapsed);
-    printf("->>>>>> %zu !! difference of number of iteration for base code of sneaky\n", iteration-145);
+    printf("->>>>>> %zu !! difference of number of iteration for base code of sneaky\n", iteration - 145);
 }
 
 /**
@@ -137,13 +139,13 @@ void generate_nice_graph(void)
     printf("Generate a graph for testing purposes (i.e.: a nice and conveniently designed graph :) )\n");
     double start = omp_get_wtime();
     initialize_graph();
-    for(int i = 0; i < GRAPH_ORDER; i++)
+    for (int i = 0; i < GRAPH_ORDER; i++)
     {
-        for(int j = 0; j < GRAPH_ORDER; j++)
+        for (int j = 0; j < GRAPH_ORDER; j++)
         {
             int source = i;
             int destination = j;
-            if(i != j)
+            if (i != j)
             {
                 adjacency_matrix[source][destination] = 1.0;
             }
@@ -160,13 +162,13 @@ void generate_sneaky_graph(void)
     printf("Generate a graph for the challenge (i.e.: a sneaky graph :P )\n");
     double start = omp_get_wtime();
     initialize_graph();
-    for(int i = 0; i < GRAPH_ORDER; i++)
+    for (int i = 0; i < GRAPH_ORDER; i++)
     {
-        for(int j = 0; j < GRAPH_ORDER - i; j++)
+        for (int j = 0; j < GRAPH_ORDER - i; j++)
         {
             int source = i;
             int destination = j;
-            if(i != j)
+            if (i != j)
             {
                 adjacency_matrix[source][destination] = 1.0;
             }
@@ -175,19 +177,18 @@ void generate_sneaky_graph(void)
     printf("%.2f seconds to generate the graph.\n", omp_get_wtime() - start);
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     // We do not need argc, this line silences potential compilation warnings.
-    (void) argc;
+    (void)argc;
     // We do not need argv, this line silences potential compilation warnings.
-    (void) argv;
+    (void)argv;
 
     printf("This program has two graph generators: generate_nice_graph and generate_sneaky_graph. If you intend to submit, your code will be timed on the sneaky graph, remember to try both.\n");
 
     // Get the time at the very start.
     double start = omp_get_wtime();
 
-    // generate_nice_graph();
     generate_sneaky_graph();
 
     /// The array in which each vertex pagerank is stored.
@@ -196,9 +197,9 @@ int main(int argc, char* argv[])
 
     // Calculates the sum of all pageranks. It should be 1.0, so it can be used as a quick verification.
     double sum_ranks = 0.0;
-    for(int i = 0; i < GRAPH_ORDER; i++)
+    for (int i = 0; i < GRAPH_ORDER; i++)
     {
-        if(i % 100 == 0)
+        if (i % 100 == 0)
         {
             printf("PageRank of vertex %d: %.6f\n", i, pagerank[i]);
         }
